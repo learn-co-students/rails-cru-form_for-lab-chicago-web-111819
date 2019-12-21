895509ebab9cfa548ca40ca92bdfd82b0038e7f1
dut-a <d.dut@live.com>
2019-12-20 18:42:44 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+puts "Beginning seeding..."
+
+artists, genres = [], []
+10.times do
+    artists << Artist.create(name: Faker::Name.name, bio: Faker::Lorem.sentences)
+    genres << Genre.create(name: Faker::Music.genre)
+end
+
+20.times do
+    Song.create(name: Faker::Music::Phish.song, artist_id: artists.sample.id, genre_id: genres.sample.id)
+end
+
+puts "Finished seeding..."
+
